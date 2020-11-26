009b64c091f18b5273e2d8f1aadce90e7a7e45e1
pealejosb <pedroalejosb@gmail.com>
2020-11-25 22:04:32 -0300

    Se agrega modelo producto y corrige modelo usuario

---
+++ models/product.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const {
+    ObjectId
+} = mongoose.Schema;
+
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 50,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 100,
+
+    },
+    quantity: {
+        type: Number,
+        required: true
+
+    },
+    format: {
+        type: String,
+        default: "KG",
+        enum: ["KG", "LT", "GR", "ML"]
+    },
+    unit: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    },
+    icon: {
+        data: Buffer,
+        contentType: String
+
+    }
+
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Product", categorySchema);
